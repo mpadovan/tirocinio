b78ee87d5653fda406b80f9a507f18e92f3e7493
Matteo Padovan <mpadovan@live.it>
2020-11-25 17:26:46 +0100

    test heroku finale

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 var express = require('express');
 const fs = require('fs');
-const csv = require('csv-parser');
+const session = require('express-session');
+const uuid = require('uuid')
+var path = require('path')
+// const csv = require('csv-parser');
 
 const app = express()
-const port = 3000
-var videos= null;
+app.use(express.json());
+app.use(session({
+    secret: 'sessione bellix',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { expires: new Date(253402300000000) }
+}))
 
-app.use(express.static('public'))
+const port = 3000
+var videos = null;
+
+process.env.PWD = process.cwd();
+app.use(express.static(path.join(process.env.PWD, 'public')));
+
+app.get('/', (req, res) => {
+    if (typeof req.session.view === "undefined") req.session.view = uuid.v4();
+    fs.readFile(path.join(process.env.PWD, '/index.html'), (err, data) => {
+        if (err) res.sendStatus(500);
+        else res.send(data.toString());
+    })
+})
 
 app.get('/videos', (req, res) => {
-  res.send(JSON.stringify(videos));
+    if (typeof req.session.view !== "undefined") {
+        if(typeof req.session.count === "undefined") req.session.count = 0;
+        res.json({ videos: videos.videos, count: req.session.count });
+    } else res.json({ videos: [], count: 0 });
+});
+
+app.post('/videos/:name', (req, res) => {
+    if (typeof req.session.view === "undefined") res.sendStatus(401)
+    fs.appendFile(
+        path.join(process.env.PWD, '/videos.csv'),
+        `${req.params.name}, ${req.session.view}, ${req.body.weight}, ${req.body.masc}, ${req.body.time}\n`,
+        (err) => {
+            req.session.count++;
+            if (err) res.json({ ok: false });
+            else res.json({ ok: true, count: req.session.count });
+        })
 });
 
 app.listen(process.env.PORT || port, () => {
-  fs.readFile('videos.json', (err, data) => {
-    videos = JSON.parse(data.toString());
-  });
-  console.log(`Example app listening at http://localhost:${port}`)
+    fs.readFile(path.join(process.env.PWD, '/videos.json'), (err, data) => {
+        videos = JSON.parse(data.toString());
+    });
+    console.log(`Example app listening at http://localhost:${port}`)
 })
